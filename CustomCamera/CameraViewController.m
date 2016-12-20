b7c47082ee478e90e34c35ede48a2ef1a1dec2d2
ibos <ibos@sma11case.local>
2016-12-20 15:41:26 +0800

    优化

--- CustomCamera/CameraViewController.m
+++ CustomCamera/CameraViewController.m
@@ -13,12 +13,9 @@
 #define ScreenWidth [UIScreen mainScreen].bounds.size.width
 #define ScreenHeight [UIScreen mainScreen].bounds.size.height
 
-
-@interface CameraViewController ()<UIGestureRecognizerDelegate>
-
-@property (weak, nonatomic) IBOutlet UIView *backView;
-
-
+@interface CameraViewController ()<UIGestureRecognizerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIView *cameraView;
+@property (weak, nonatomic) IBOutlet UIView *circleView;
 /**
  *  捕获设备
  */
@@ -49,12 +46,9 @@
  */
 @property(nonatomic,assign)CGFloat effectiveScale;
 
+
 @property (nonatomic, strong) UIView *focusView;
-
-@property (nonatomic, strong) UIImageView *photoView;
-
-
-@property (nonatomic, assign) BOOL isFrontCamera;
+@property (nonatomic, weak) UIImageView *photoView;
 @end
 
 @implementation CameraViewController
@@ -63,20 +57,24 @@
     if (!_focusView) {
         _focusView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 80, 80)];
         _focusView.layer.borderWidth = 1.0;
-        _focusView.layer.borderColor =[UIColor greenColor].CGColor;
+        _focusView.layer.borderColor = [UIColor colorWithRed:0.251 green:0.694 blue:1.000 alpha:1.000].CGColor;
         _focusView.backgroundColor = [UIColor clearColor];
         _focusView.hidden = YES;
-        [self.backView addSubview:_focusView];
+        [self.cameraView addSubview:_focusView];
     }
     return _focusView;
 }
 
 - (UIImageView *)photoView {
     if (!_photoView) {
-        _photoView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight - 120)];
-        _photoView.contentMode = UIViewContentModeScaleAspectFill;
-        _photoView.hidden = YES;
-        [self.backView addSubview:_photoView];
+        UIImageView *photoView = [[UIImageView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        photoView.contentMode = UIViewContentModeScaleAspectFit;
+        photoView.backgroundColor = [UIColor blackColor];
+        photoView.userInteractionEnabled = YES;
+        photoView.hidden = YES;
+        [photoView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hiddenPhoto)]];
+        [self.view addSubview:photoView];
+        _photoView = photoView;
     }
     return _photoView;
 }
@@ -87,47 +85,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-   
-    
     [self initAVCaptureSession];
-    [self setUpGesture];
     
-    _isFrontCamera = NO;
     self.effectiveScale = self.beginGestureScale = 1.0f;
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(focusGesture:)];
+    [self.cameraView addGestureRecognizer:tapGesture];
 }
-
-- (BOOL)prefersStatusBarHidden {
-    return YES;
-}
-
-- (void)viewWillAppear:(BOOL)animated{
-    [super viewWillAppear:YES];
-     self.navigationController.navigationBar.hidden = YES;
-    if (self.session) [self.session startRunning];
-}
-
-
-- (void)viewWillDisappear:(BOOL)animated{
-    [super viewDidDisappear:YES];
-     self.navigationController.navigationBar.hidden = NO;
-    if (self.session) [self.session stopRunning];
-}
-
 
 #pragma mark private method
 - (void)initAVCaptureSession{
-    
     self.session = [[AVCaptureSession alloc] init];
-    
     self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
     //更改这个设置的时候必须先锁定设备，修改完后再解锁，否则崩溃
     [self.device lockForConfiguration:nil];
     [self.device setFlashMode:AVCaptureFlashModeAuto];//闪光灯
+    //设置帧数
+    self.device.activeVideoMinFrameDuration  = CMTimeMake(1, 15);
     [self.device unlockForConfiguration];
     
     self.videoInput = [[AVCaptureDeviceInput alloc] initWithDevice:self.device error:nil];
-
+    
     self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
     //输出设置。AVVideoCodecJPEG   输出jpeg格式图片
     self.stillImageOutput.outputSettings = @{AVVideoCodecKey: AVVideoCodecJPEG};
@@ -142,118 +121,102 @@
     
     //初始化预览图层
     self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
-    self.previewLayer.frame = CGRectMake(0, 0, ScreenWidth, ScreenHeight - 120);
     [self.previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
-    self.backView.layer.masksToBounds = YES;
-    [self.backView.layer addSublayer:self.previewLayer];
+    self.previewLayer.frame = CGRectMake(0, 0, ScreenWidth, ScreenHeight - 120);
+    self.cameraView.layer.masksToBounds = YES;
+    [self.cameraView.layer addSublayer:self.previewLayer];
+    
+    [self.session startRunning];
 }
 
 
-//- (AVCaptureVideoOrientation)avOrientationForDeviceOrientation:(UIDeviceOrientation)deviceOrientation {
-//    AVCaptureVideoOrientation result = (AVCaptureVideoOrientation)deviceOrientation;
-//    if (deviceOrientation == UIDeviceOrientationLandscapeLeft)
-//        return AVCaptureVideoOrientationLandscapeRight;
-//    else if ( deviceOrientation == UIDeviceOrientationLandscapeRight)
-//        return AVCaptureVideoOrientationLandscapeLeft;
-//    return result;
-//}
-
-- (void)setUpGesture{
-    UIPinchGestureRecognizer *pinch = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinchGesture:)];
-    pinch.delegate = self;
-    [self.backView addGestureRecognizer:pinch];
-    
-    
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(focusGesture:)];
-    [self.backView addGestureRecognizer:tapGesture];
+- (void)hiddenPhoto {
+    self.photoView.hidden = YES;
+    [self.session startRunning];
 }
 
 
-
-#pragma mark respone method
-- (IBAction)changeCamera:(UIButton *)sender {
-    AVCaptureDevicePosition desiredPosition = _isFrontCamera?AVCaptureDevicePositionBack: AVCaptureDevicePositionFront;
-    for (AVCaptureDevice *d in [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo]) {
-        if (d.position == desiredPosition) {
-            [self.previewLayer.session beginConfiguration];
-            AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:d error:nil];
-            for (AVCaptureInput *oldInput in self.previewLayer.session.inputs) {
-                [[self.previewLayer session] removeInput:oldInput];
-            }
-            [self.previewLayer.session addInput:input];
-            [self.previewLayer.session commitConfiguration];
-            break;
-        }
+- (IBAction)takePhoto {
+    //去掉快门声音
+    static SystemSoundID soundID = 0;
+    if (soundID == 0) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"photoShutter2" ofType:@"caf"];
+        NSURL *filePath = [NSURL fileURLWithPath:path isDirectory:NO];
+        AudioServicesCreateSystemSoundID((__bridge CFURLRef)filePath, &soundID);
     }
-    _isFrontCamera = !_isFrontCamera;
-}
+    AudioServicesPlaySystemSound(soundID);
+    
+    AVCaptureConnection *stillImageConnection = [self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo];
+    [stillImageConnection setVideoScaleAndCropFactor:self.effectiveScale];
+    
+    [self.stillImageOutput captureStillImageAsynchronouslyFromConnection:stillImageConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
 
-- (IBAction)cancel {
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
-
-
-- (IBAction)takePhotoButtonClick:(UIButton *)sender {
-    self.photoView.hidden = sender.selected;
-    self.photoView.image = nil;
-    if (!sender.selected) {
-        AVCaptureConnection *stillImageConnection = [self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo];
-//        UIDeviceOrientation curDeviceOrientation = [[UIDevice currentDevice] orientation];
-//        AVCaptureVideoOrientation avcaptureOrientation = [self avOrientationForDeviceOrientation:curDeviceOrientation];
-//        [stillImageConnection setVideoOrientation:avcaptureOrientation];
-        [stillImageConnection setVideoScaleAndCropFactor:self.effectiveScale];
+        NSData *jpegData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+        UIImage *image = [UIImage imageWithData:jpegData];
         
-        [self.stillImageOutput captureStillImageAsynchronouslyFromConnection:stillImageConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
-            
-            NSData *jpegData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
-            UIImage *image = [self normalizedImage:[UIImage imageWithData:jpegData]];
-            self.photoView.image = image;//[self imageFromImage:image];
-        }];
-    }
-    sender.selected = !sender.selected;
+        if (self.session.isRunning) [self.session stopRunning];
+        self.photoView.image = [self tailorImage:image];
+        self.photoView.hidden = NO;
+    }];
 }
 
-- (UIImage *)normalizedImage:(UIImage *)image {
-    if (image.imageOrientation == UIImageOrientationUp) return image;
-    UIGraphicsBeginImageContextWithOptions(image.size, NO, image.scale);
-    [image drawInRect:(CGRect){0, 0, image.size}];
-    UIImage *normalizedImage = UIGraphicsGetImageFromCurrentImageContext();
+
+//裁剪图片
+- (UIImage *)tailorImage:(UIImage *)image {
+    CGFloat scale = image.size.width / _cameraView.frame.size.width;
+    CGFloat showImageH = image.size.height / scale;
+    CGFloat offsetY = (showImageH - _cameraView.frame.size.height) * 0.5;
+    CGFloat factY = (_circleView.frame.origin.y + offsetY) * scale;
+    CGRect rect = CGRectMake(factY, _circleView.frame.origin.x * scale, _circleView.frame.size.height * scale, _circleView.frame.size.width * scale);
+    
+    CGImageRef oriImageRef = image.CGImage;
+    CGImageRef tailorImageRef = CGImageCreateWithImageInRect(oriImageRef, rect);
+    UIImage *tailorImage = [UIImage imageWithCGImage:tailorImageRef];
+    
+    //压缩图片并黑白处理，减小图片体积
+    tailorImage = [self imageCompressImage:tailorImage targetWidth:800];
+    tailorImage = [self convertImageToGreyScale:tailorImage];
+    
+    return tailorImage;
+}
+
+
+//压缩图片
+- (UIImage *)imageCompressImage:(UIImage *)sourceImage targetWidth:(CGFloat)defineWidth {
+    
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = defineWidth;
+    CGFloat targetHeight = (targetWidth / width) * height;
+    
+    UIGraphicsBeginImageContext(CGSizeMake(targetWidth, targetHeight));
+    [sourceImage drawInRect:CGRectMake(0,0,targetWidth, targetHeight)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    return normalizedImage;
+    
+    return newImage;
+    
 }
 
-//- (UIImage *)imageFromImage:(UIImage *)image{
-//    CGFloat height = ScreenWidth * image.size.height / image.size.width - ScreenHeight;
-//    CGFloat y = height / ScreenWidth * image.size.width;
-//    CGRect rect = CGRectMake(0, y, image.size.width, (ScreenHeight - 120) / ScreenWidth * image.size.width);
-//    CGImageRef sourceImageRef = image.CGImage;
-//    CGImageRef newImageRef = CGImageCreateWithImageInRect(sourceImageRef, rect);
-//    UIImage *newImage = [UIImage imageWithCGImage:newImageRef];
-//    CGImageRelease(newImageRef);
-//    return newImage;
-//}
-
-#pragma mark gestureRecognizer delegate
-- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
-    if ( [gestureRecognizer isKindOfClass:[UIPinchGestureRecognizer class]] ) {
-        self.beginGestureScale = self.effectiveScale;
-    }
-    return YES;
-}
-
-//缩放手势 用于调整焦距
-- (void)handlePinchGesture:(UIPinchGestureRecognizer *)recognizer{
-    self.effectiveScale = self.beginGestureScale * recognizer.scale;
-    if (self.effectiveScale < 1.0) self.effectiveScale = 1.0;
-    CGFloat maxScaleAndCropFactor = [[self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo] videoMaxScaleAndCropFactor];
-    if (self.effectiveScale > maxScaleAndCropFactor) self.effectiveScale = maxScaleAndCropFactor;
-    self.previewLayer.affineTransform = CGAffineTransformMakeScale(self.effectiveScale, self.effectiveScale);
+//黑白化
+- (UIImage *) convertImageToGreyScale:(UIImage*) image {
+    CGRect imageRect = CGRectMake(0, 0, image.size.width, image.size.height);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+    CGContextRef context = CGBitmapContextCreate(nil, image.size.width, image.size.height, 8, 0, colorSpace, kCGImageAlphaNone);
+    CGContextDrawImage(context, imageRect, [image CGImage]);
+    CGImageRef imageRef = CGBitmapContextCreateImage(context);
+    UIImage *newImage = [UIImage imageWithCGImage:imageRef];
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    CFRelease(imageRef);
+    return newImage;
 }
 
 
 - (void)focusGesture:(UITapGestureRecognizer*)gesture{
     CGPoint point = [gesture locationInView:gesture.view];
-    CGSize size = self.backView.bounds.size;
+    CGSize size = self.cameraView.bounds.size;
     CGPoint focusPoint = CGPointMake( point.y / size.height ,1 - point.x / size.width);
     if ([self.device lockForConfiguration:nil]) {
         
